bc176a46e0376dbf2deddc255928fc87cf497dbb
Takeru Ohta <phjgt308@gmail.com>
2023-06-02 19:52:33 +0900

    Add main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use daberu::ChatGpt;
+use orfail::OrFail;
+
+/// A tool for conversing with ChatGPT.
+#[derive(Debug, Parser)]
+#[command(version)]
+struct Args {
+    #[clap(flatten)]
+    chatgpt: ChatGpt,
+}
+
+fn main() -> orfail::Result<()> {
+    let args = Args::parse();
+    args.chatgpt.call().or_fail()?;
+    Ok(())
 }

4079fb4edd5dbafb24e7563abd6dae0b724bf80f
Takeru Ohta <phjgt308@gmail.com>
2025-04-01 09:38:47 +0900

    Update noargs dependency to 0.2.0 and update CLI help text formatting

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,26 @@ fn main() -> noargs::Result<()> {
         println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
         return Ok(());
     }
-    if noargs::HELP_FLAG.take(&mut args).is_present() {
-        args.metadata_mut().help_mode = true;
-    }
+    noargs::HELP_FLAG.take_help(&mut args);
 
     let command = Command {
         openai_api_key: noargs::opt("openai-api-key")
             .ty("STRING")
             .env("OPENAI_API_KEY")
-            .doc("OpenAI API key.")
+            .doc("OpenAI API key")
             .take(&mut args)
             .parse_if_present()?,
         anthropic_api_key: noargs::opt("anthropic-api-key")
             .ty("STRING")
             .env("ANTHROPIC_API_KEY")
-            .doc("Anthropic API key.")
+            .doc("Anthropic API key")
             .take(&mut args)
             .parse_if_present()?,
         log: noargs::opt("log")
             .ty("PATH")
             .doc(concat!(
-                "Path to log file for saving conversation history. \n",
+                "Path to log file for saving conversation history\n",
+                "\n",
                 "If the file already exists, its contents are included\n",
                 "in subsequent conversations."
             ))
@@ -40,8 +39,9 @@ fn main() -> noargs::Result<()> {
             .ty("PATH")
             .env("DABERU_ONESHOT_LOG_PATH")
             .doc(concat!(
-                "Specifies a path for a \"one-shot\" conversation log file which\n",
-                "will overwrite any existing file content upon opening.\n",
+                "Specifies a path for a \"one-shot\" conversation log file\n",
+                "\n",
+                "Upon opening the log file, any existing file content will be truncated.\n",
                 "If `--log` is specified, this option will be ignored."
             ))
             .take(&mut args)
@@ -51,19 +51,20 @@ fn main() -> noargs::Result<()> {
             .ty("STRING")
             .default("gpt-4o")
             .env("DABERU_MODEL")
-            .doc("Model name.")
+            .doc("Model name")
             .take(&mut args)
             .parse()?,
         system: noargs::opt("system")
             .short('s')
             .ty("STRING")
-            .doc("System message.")
+            .doc("System message")
             .take(&mut args)
             .parse_if_present()?,
         gist: noargs::opt("gist")
             .ty("new | EXISTING_GIST_ID")
             .doc(concat!(
-                "Save the output to GitHub Gist.\n",
+                "Save the output to GitHub Gist\n",
+                "\n",
                 "If `EXISTING_GIST_ID` is specified,\n",
                 "load the log from the Gist entry and update the entry."
             ))

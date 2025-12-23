24086c854c79ec062d38c0cbfcf7657931bc06f5
Takeru Ohta <phjgt308@gmail.com>
2025-12-23 09:22:54 +0900

    fix: return early when help mode is enabled in last subcommand

--- src/subcommand_last.rs
+++ src/subcommand_last.rs
@@ -10,6 +10,10 @@ pub fn run(args: &mut noargs::RawArgs) -> noargs::Result<()> {
         .doc("Path to log file containing the conversation history")
         .take(args)
         .then(|a| a.value().parse())?;
+    if args.metadata().help_mode {
+        return Ok(());
+    }
+
     let log = MessageLog::load(log)?;
     if let Some(m) = log.messages.last() {
         println!("{}", m.content);

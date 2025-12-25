e6647f1e1d0371dd1a3fffc19898c3b5c02684e0
Takeru Ohta <phjgt308@gmail.com>
2025-12-25 21:42:50 +0900

    chore: reorder subcommand_clean_files module declaration

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod curl;
 pub mod json;
 pub mod message;
 pub mod resource;
+pub mod subcommand_clean_files;
 pub mod subcommand_create_skill;
 pub mod subcommand_delete_file;
 pub mod subcommand_delete_skill;
@@ -13,5 +14,3 @@ pub mod subcommand_get_skill;
 pub mod subcommand_last;
 pub mod subcommand_list_files;
 pub mod subcommand_list_skills;
-pub mod subcommand_clean_files;
-
